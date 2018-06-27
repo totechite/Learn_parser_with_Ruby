ea563e51723e47560eef766d5a5c439bb6ab659a
totechite <yutan.h9@gmail.com>
2018-06-27 22:30:14 +0900

    cleaned main.rb

--- main.rb
+++ main.rb
@@ -3,19 +3,12 @@ require './lib/index'
 def main
   f = File.new "sample.md"
   lexer = ListLexer.new f
-  token = lexer.next_token
-  puts token.to_s
-
-  while token.to_s[:type]!="<EOF>"
-  token = lexer.next_token
-  puts token.to_s
+  frag = true
+  while frag
+    token = lexer.next_token
+    puts token.to_s
+    frag = token.to_s[:type]!="<EOF>"
   end
-
-end
-
-def test
-  f = File.new "./sample.md"
-  lexer = ListLexer.new f
 
 end
 

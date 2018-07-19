a8ac326a6ced1d78bf386164bb11e120a7b80284
totechite <yutan.h9@gmail.com>
2018-07-19 21:38:38 +0900

    readable sharp

--- lib/list_lexer.rb
+++ lib/list_lexer.rb
@@ -1,25 +1,33 @@
 class ListLexer < Lexer
-  @tokenNames = %w[n/a <EOF> NAME COMMA L_BRACKET R_BRACEKT HEADING LIST]
+  @tokenNames = %w[n/a <EOF> NAME COMMA L_BRACKET R_BRACEKT SHARP LIST LINE_FEED]
   @NAME = 2
   @COMMA = 3
   @L_BRACKET = 4
   @R_BRACKET = 5
+  @SHARP = 6
+  @LINE_FEED = 7
 
   def initialize(text)
-    @tokenNames = %w[n/a <EOF> NAME COMMA L_BRACKET R_BRACEKT HEADING LIST]
+    @tokenNames = %w[n/a <EOF> NAME COMMA L_BRACKET R_BRACEKT SHARP LIST LINE_FEED]
     @NAME = 2
     @COMMA = 3
     @L_BRACKET = 4
     @R_BRACKET = 5
+    @SHARP = 6
+    @LINE_FEED = 7
     super text
   end
 
   class << self
-    attr_reader :tokenNames, :NAME, :COMMA, :L_BRACKET, :R_BRACKET
+    attr_reader :tokenNames, :NAME, :COMMA, :L_BRACKET, :R_BRACKET, :SHARP, :LINE_FEED
   end
 
   def is_letter?
-    (@char =~ /[a-z]|[A-Z]/) != nil
+    (@char =~ /[a-z]|[A-Z]/) != nil 
+  end
+
+  def is_sharp?
+    @char == "#"
   end
 
   def next_token
@@ -28,7 +36,7 @@ class ListLexer < Lexer
       case @char
       when "\s" || "\t" || "\n" || "\r" || "\r\n"
         white_space
-        continue
+        next
       when ','
         consume
         return Token.new(@COMMA, ',')
@@ -38,6 +46,8 @@ class ListLexer < Lexer
       when ']'
         consume
         return Token.new(@R_BRACKET, ']')
+      when '#'
+        return sharp_token if is_sharp?
       else
         begin
           return name_token if is_letter?
@@ -55,11 +65,23 @@ class ListLexer < Lexer
     frag = true
     while frag
       str += @char
-      consume
       frag = is_letter?
+      consume
     end
     Token.new @NAME, str
   end
+
+  def sharp_token
+    str = ''
+    frag = true
+    while frag
+      str += @char
+      consume
+      frag = is_sharp?
+    end
+    Token.new @SHARP, str
+  end
+
 
   def white_space
     consume while @char == ("\s" || "\t" || "\n" || "\r" || "\r\n")
